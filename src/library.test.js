1136dc0bc771600e6ed3f4eb1c5ef1a4fd223f3d
Antonin Delpeuch <antonin@delpeuch.eu>
2020-04-04 18:41:57 +0200

    Better testing

--- src/library.test.js
+++ src/library.test.js
@@ -3,6 +3,7 @@ import DiagramLibrary from './library.js' ;
 import SheetDiagram from './sheet_diagram.js' ;
 
 let diag = new SheetDiagram([1, 2], [{swap: 0}]);
+let diag2 = new SheetDiagram([1, 2], [{swap: 0},{swap: 0}]);
 
 test('saves and retrieves diagrams', function() {
         let lib = new DiagramLibrary();
@@ -20,6 +21,28 @@ test('saves and retrieves diagrams', function() {
         lib.remove('myDiagram');
 
         expect(lib.size()).toEqual(0);
+});
+
+test('override diagram', function() {
+	let lib = new DiagramLibrary();
+
+	lib.addDiagram('diag', diag);
+
+	expect(lib.getDiagram('diag').nbVertices()).toEqual(1);
+
+	lib.addDiagram('diag', diag2);
+
+	expect(lib.size()).toEqual(1);
+	expect(lib.getDiagram('diag').nbVertices()).toEqual(2);
+});
+
+test('store two diagrams', function() {
+	let lib = new DiagramLibrary();
+
+	lib.addDiagram('diag', diag);
+	lib.addDiagram('diag2', diag2);
+
+	expect(lib.size()).toEqual(2);
 });
 
 test('imports and exports in JSON', function() {
